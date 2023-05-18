7eb62cc88aa8976bc666a67bd12afbd54222d41f
mobin <mobintahmasbi13801202@gmail.com>
2023-05-18 22:37:02 +0330

    start to add authentication to project

--- app/middlewares/index.js
+++ app/middlewares/index.js
@@ -1,6 +1,9 @@
 const bdp = require('body-parser')
 const corse = require('cors')
+const auth = require('./athorize')
+
 module.exports = (app) => {
+  app.use('/api/v1/users', auth)
   app.use(corse())
   app.use(bdp.json())
 }
