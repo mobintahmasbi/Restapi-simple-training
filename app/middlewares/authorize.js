3545018dcfa9200b0f8c3dd2c8e2bfb430b30e92
mobin <mobintahmasbi13801202@gmail.com>
2023-05-19 12:18:54 +0330

    add authorization to project

--- app/middlewares/authorize.js
+++ app/middlewares/authorize.js
@@ -1,5 +1,18 @@
+const jwt = require('jsonwebtoken')
+const privateKey =
+  'yeki bod yeki nabod gheir az khodaye mehrabon hichkas nabod'
+
 const auth = (req, res, next) => {
-  next()
+  try {
+    const { token } = req.cookies
+    if (token === undefined || token === null) {
+      return res.send('you have to send token with cookie')
+    }
+    jwt.verify(token, privateKey)
+    next()
+  } catch (e) {
+    return res.send('invalid token')
+  }
 }
 
 module.exports = auth
